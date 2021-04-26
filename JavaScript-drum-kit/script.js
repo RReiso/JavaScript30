01f93c0812d92f55c0d4d1e9bf433afc0ca7cc0c
RReiso <ruta.littlemore@gmail.com>
2021-04-26 16:07:35 -0400

    play sound when lowercase or uppercase key is pressed

--- JavaScript-drum-kit/script.js
+++ JavaScript-drum-kit/script.js
@@ -1,21 +1,25 @@
 window.addEventListener("keydown", (e) => {
-	const audio = document.querySelector(`audio[data-key="${e.key}"]`); //find audio with specific data key
-	const key = document.querySelector(`.key[data-key="${e.key}"]`); //find element with .key class with specific data-key
+  chosenKey = e.key.toLowerCase();
+	const audio = document.querySelector(`audio[data-key="${chosenKey}"]`); //find audio with specific data key
+	const key = document.querySelector(`.key[data-key="${chosenKey}"]`); //find element with .key class with specific data-key
 	if (!audio) return;
 	playSound(audio, key);
 });
 
-// window.addEventListener("click", (e) => {
-// 	clicked = e.target.closest(".key");
-// 	if (!clicked) return;
-// 	const audio = document.querySelector(
-// 		`audio[data-key="${clicked.getAttribute("data-key")}"]`
-// 	);
-// 	const key = document.querySelector(
-// 		`.key[data-key="${clicked.getAttribute("data-key")}"]`
-// 	);
-//   playSound(audio,key);
-// });
+window.addEventListener("mousedown", findClickedElements);//for desktop, triggers also with left and middle mouse button.
+window.addEventListener("touchstart", findClickedElements, false); //for touchscreens, "click" event does not work on Safari if the element is "div"
+
+function findClickedElements(e) {
+	clicked = e.target.closest(".key");
+	if (!clicked) return;
+	const audio = document.querySelector(
+		`audio[data-key="${clicked.getAttribute("data-key")}"]`
+	);
+	const key = document.querySelector(
+		`.key[data-key="${clicked.getAttribute("data-key")}"]`
+	);
+	playSound(audio, key);
+}
 
 function playSound(audio, key) {
 	audio.currentTime = 0; //if audio is playing, set it back to 0.
@@ -29,20 +33,3 @@ function removeTransition(e){
 	this.classList.remove("playing"); //remove class once the longest transition (here:transform) has finished transitioning. "this" referes to key.
 }
 
-
-window.addEventListener("click", touchStart);
-window.addEventListener("touchstart", touchStart, false);
-
-console.log("jauns2");
-
-function touchStart(e) {
-	clicked = e.target.closest(".key");
-	if (!clicked) return;
-	const audio = document.querySelector(
-		`audio[data-key="${clicked.getAttribute("data-key")}"]`
-	);
-	const key = document.querySelector(
-		`.key[data-key="${clicked.getAttribute("data-key")}"]`
-	);
-	playSound(audio, key);
-}
