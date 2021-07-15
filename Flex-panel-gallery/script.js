103ec53b8682c7e87b6b7e61bddcb5cfd6b37c18
RReiso <rutareiso@protonmail.com>
2021-07-15 00:05:35 -0400

    Set up gallery

--- Flex-panel-gallery/script.js
+++ Flex-panel-gallery/script.js
@@ -4,12 +4,12 @@ function toggleOpen() {
 	this.classList.toggle("open");
 }
 
-// function toggleActive(e) {
-// 	console.log(e.propertyName);
-// 	if (e.propertyName.includes("flex")) {
-// 		this.classList.toggle("open-active");
-// 	}
-// }
+function toggleActive(e) {
+	console.log(e.propertyName);
+	if (e.propertyName.includes("flex")) {
+		this.classList.toggle("open-active");
+	}
+}
 
 panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
 panels.forEach((panel) =>
