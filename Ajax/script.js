8a5f80262a7d9baf49ec8717dafc7df5e5e57abe
RReiso <rutareiso@protonmail.com>
2021-07-23 22:07:00 -0400

    Add styles

--- Ajax/script.js
+++ Ajax/script.js
@@ -1,91 +1,83 @@
+let URI = "https://api.teleport.org/api/urban_areas/";
 
+let cities = [];
+fetch(URI)
+	.then((response) => response.json())
+	.then((data) => {
+		cities = [...data._links["ua:item"]];
+    console.log(cities)
+	});
 
-    let endpoint = "https://api.teleport.org/api/urban_areas/";
-	
-		let cities = [];
-		fetch(endpoint)
-			.then((response) => response.json())
-			.then((data) => {
-				cities=[...data._links["ua:item"]]
-			
-        // console.log(cities)
-			});
-     
+const searchInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+let image = document.querySelector("img");
+let cityTitle = document.querySelector(".city-title");
 
-      const searchInput = document.querySelector(".search");
-			const suggestions = document.querySelector(".suggestions");
-      let image = document.querySelector("img");
-      let cityTitle = document.querySelector(".city-title");
+// searchInput.addEventListener("change", displayMatches);
+suggestions.addEventListener("click", displayCityInfo);
+searchInput.addEventListener("input", displayMatches); //The input event works on keyboard devices and with touchscreen devices. Input event will not be triggered by all keys (CTRL, ALT & co. will not fire the event
 
-      // searchInput.addEventListener("change", displayMatches);
-      suggestions.addEventListener("click", displayCityInfo)
-      searchInput.addEventListener("input", displayMatches); //The input event works on keyboard devices and with touchscreen devices. Input event will not be triggered by all keys (CTRL, ALT & co. will not fire the event
+function findMatches(wordToMatch, cities) {
+	return cities.filter((city) => {
+		const regex = new RegExp(wordToMatch, "gi");
+		return city.name.match(regex);
+	});
+}
 
-      function findMatches(wordToMatch, cities) {
-				return cities.filter((city) => {
-					const regex = new RegExp(wordToMatch, "gi");
-					return city.name.match(regex) ;
-				});
-			}
+function displayMatches() {
+	cityTitle.innerHTML = "";
+	image.setAttribute("src", "");
+	image.setAttribute("alt", "");
 
-      function displayMatches() {
-        cityTitle.innerHTML="";
-        image.setAttribute("src", "");
-        image.setAttribute("alt","");
+	const matchArray = findMatches(this.value, cities);
+	// console.log(matchArray);
+	const html = matchArray
+		.map((city) => {
+			const regex = new RegExp(this.value, "gi");
+			const cityName = city.name.replace(
+				regex,
+				`<span class="hl">${this.value}</span>`
+			);
 
-				const matchArray = findMatches(this.value, cities);
-        // console.log(matchArray);
-				const html = matchArray.map((city) => {
-						const regex = new RegExp(this.value, "gi");
-						const cityName = city.name.replace(
-							regex,
-							`<span class="hl">${this.value}</span>`
-						);
-						
-						return `
+			return `
       <li>
         <span class="name" data-city="${city.name}">${cityName}</span>
       
       </li>
     `;
-					})
-					.join("");
-				suggestions.innerHTML = html;
-      
-    }
+		})
+		.join("");
+	suggestions.innerHTML = html;
+}
 
-      function displayCityInfo(event) {
-				suggestions.innerHTML = "";
-				const clickedElement = event.target.closest("li");
-				let cityName = clickedElement
-					.querySelector(".name")
-					.getAttribute("data-city")
-					.toLowerCase()
-					.split(" ")
-					.join("-");
-				console.log(cityName);
+function displayCityInfo(event) {
+	suggestions.innerHTML = "";
+	const clickedElement = event.target.closest("li");
+	let cityName = clickedElement
+		.querySelector(".name")
+		.getAttribute("data-city")
 
-				fetch(
-					`https://api.teleport.org/api/urban_areas/slug:${cityName
-						}/`
-				)
-					.then((response) => response.json())
-					.then((data) => {
-						// console.log(data._links);
-						cityTitle.innerHTML = `${
-							data.full_name
-						}`;
+		.toLowerCase()
+		.replace(/\./g, "") //replace all dots
+		.split(/[\s,]+/)
+		.join("-");
+	getFullName(cityName);
+	getCityImage(cityName);
+}
 
-           
-					});
+function getFullName(cityName) {
+	fetch(`${URI}slug:${cityName}/`)
+		.then((response) => response.json())
+		.then((data) => {
+			cityTitle.innerHTML = `${data.full_name}`;
+		});
+}
 
-           fetch(
-							`https://api.teleport.org/api/urban_areas/slug:${cityName}/images/`
-						)
-							.then((response) => response.json())
-							.then((data) => {
-							
-								image.setAttribute("src", data.photos[0].image.mobile);
-								image.setAttribute("alt", `${cityName}`);
-							});
-			}
+function getCityImage(cityName) {
+	fetch(`${URI}slug:${cityName}/images/`)
+		.then((response) => response.json())
+		.then((data) => {
+			image.setAttribute("src", data.photos[0].image.mobile);
+			image.setAttribute("alt", `${cityName}`);
+		});
+}
